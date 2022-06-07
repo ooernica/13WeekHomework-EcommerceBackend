20a7919acbd5e7ee074b9d24e326af0f71e008dc
Erna Lukac <e.lukac4@gmail.com>
2022-06-07 17:32:09 -0600

    added category and product js models

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,10 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    catergory_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+  }
   },
   {
     sequelize,
